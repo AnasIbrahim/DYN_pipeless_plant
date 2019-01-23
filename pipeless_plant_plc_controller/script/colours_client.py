ab29ee60f8cb91b7581627900a5831064a1f84dd
mughees-wyne <mughees.wyne@yahoo.com>
2019-01-23 01:29:19 +0100

    previous services removed

--- pipeless_plant_plc_controller/script/colours_client.py
+++ pipeless_plant_plc_controller/script/colours_client.py
@@ -3,34 +3,34 @@
 import sys
 import rospy
 
-from plc_module.srv import *
+from pipeless_plant_plc_controller.srv import *
 
-def colours_client(v1,v1_time,v2,v2_time,v3,v3_time,v4,v4_time,close_all):
-    rospy.wait_for_service('colour_valves')
+def colours_client(op_id,v_open,v_time,v_close):
+    rospy.wait_for_service('TCP_PLC_Data')
     try:
-        colour_vals = rospy.ServiceProxy('colour_valves',colours)
-        colour1 = colour_vals(v1,v1_time,v2,v2_time,v3,v3_time,v4,v4_time,close_all)
+        tcp_srv = rospy.ServiceProxy('TCP_PLC_Data',tcp_plc)
+        colour1 = tcp_srv(op_id,v_open,v_time,v_close,0,0,0)
         return colour1.status
     except rospy.ServiceException as e:
         print("Service call failed: %s" % e)
 
 def usage():
-    return "%s [V1_Open V1_Time V2_Open V2_Time V3_Open V3_Time V4_Open V4_Time Close_Valves]"%sys.argv[0]
+    return "%s [Operation ID, Valve_Open Valve_Time Valve_Close(Optional)]"%sys.argv[0]
 
 if __name__ == "__main__":
-    if len(sys.argv) == 10:
-        v1 = bool(int(sys.argv[1]))
-        v1_time = int(sys.argv[2])
-        v2 = bool(int(sys.argv[3]))
-        v2_time = int(sys.argv[4])
-        v3 = bool(int(sys.argv[5]))
-        v3_time = int(sys.argv[6])
-        v4 = bool(int(sys.argv[7]))
-        v4_time = int(sys.argv[8])
-        close_all = bool(int(sys.argv[9]))
-        colours_client(v1,v1_time,v2,v2_time,v3,v3_time,v4,v4_time,close_all)
+    if len(sys.argv) == 5:
+        op_id = int(sys.argv[1])
+        v_open = int(sys.argv[2])
+        v_time = int(sys.argv[3])
+        v_close = int(sys.argv[4])
+        colours_client(op_id,v_open,v_time,v_close)
+    elif len(sys.argv) == 4:
+        op_id = int(sys.argv[1])
+        v_open = int(sys.argv[2])
+        v_time = int(sys.argv[3])
+        colours_client(op_id,v_open,v_time,0)
     elif len(sys.argv) == 1:
-        colours_client(0,0,0,0,0,0,0,0,0)
+        colours_client(0,0,0,0)
     else:
         print(usage())
         sys.exit(1)
